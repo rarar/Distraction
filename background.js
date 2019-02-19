b5cf0611be52c4bd68c0240ead75eec9c5c5579b
rarar <me@rarar.com>
2019-02-18 18:08:57 -0800

    storing tab change count in backend

--- background.js
+++ background.js
@@ -1,13 +1,6 @@
-let date = Date.now();
-
-chrome.runtime.onInstalled.addListener(function() {
-  console.log("on installed called");
-  chrome.tabs.getAllInWindow(function(tabs) {
-    console.log("num tabs = " + tabs.length);
-  })
-});
-
+let counter = 0;
 chrome.tabs.onSelectionChanged.addListener(function() {
   console.log("tab changed");
-  let timeOnTab = Date.now() - date;
+  counter++;
+  console.log("tab change count = " + counter);
 })
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 let time = Date.now() / 1000;
+
 chrome.tabs.getAllInWindow(function(tabs) {
     document.getElementById("tab_count").innerHTML = tabs.length;
-    document.getElementById("time").innerHTML = time;
 });
