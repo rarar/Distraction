de92f5195304533b2f4a29e976db73663538a503
rarar <me@rarar.com>
2019-02-21 18:27:57 -0800

    interval array added

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
 let counter = 0;
+let oldTime = new Date();
+let newTime;
+let timeIntervals = [];
 chrome.tabs.onSelectionChanged.addListener(function() {
   console.log("tab changed");
+  newTime = new Date();
+  let intervalToPush = newTime - oldTime;
+  timeIntervals.push(intervalToPush);
+  oldTime = newTime;
+
+  console.log("Interval = " + intervalToPush);
   counter++;
   console.log("tab change count = " + counter);
   chrome.runtime.onMessage.addListener(
--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-let time = Date.now() / 1000;
-
 chrome.tabs.getAllInWindow(function(tabs) {
     document.getElementById("tab_count").innerHTML = tabs.length;
 });
