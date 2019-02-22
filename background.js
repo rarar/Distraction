2b27aad16366170d49f038c27cf2eea82bda0c50
rarar <me@rarar.com>
2019-02-21 17:30:31 -0800

    effectively passing data from background.js to popup.js

--- background.js
+++ background.js
@@ -3,4 +3,19 @@ chrome.tabs.onSelectionChanged.addListener(function() {
   console.log("tab changed");
   counter++;
   console.log("tab change count = " + counter);
-})
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      // listen for messages
+      localStorage.setItem(request.key, counter);
+      if (request.type == 'fromPopup') {
+        let data = JSON.parse(localStorage.getItem(request.key));
+        sendResponse({
+          data: counter
+        });
+      }
+      console.log("listening");
+    // should be included by default
+    return true;
+    }
+  );
+});
--- popup.js
+++ popup.js
@@ -3,3 +3,20 @@ let time = Date.now() / 1000;
 chrome.tabs.getAllInWindow(function(tabs) {
     document.getElementById("tab_count").innerHTML = tabs.length;
 });
+
+
+  chrome.runtime.sendMessage({
+      type: "fromPopup"
+    },
+    function(response) {
+      if (response) {
+        displayData(response.data)
+      } else {
+        displayData(0);
+      }
+    }
+  );
+
+function displayData(data) {
+  document.getElementById("onChange").innerHTML = data;
+}
